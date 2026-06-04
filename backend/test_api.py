771a6af86e0de101be5a0f55636a167c43342518
Sujit224 <saisujit300@gmail.com>
2026-06-04 11:01:27 +0530

    chore: remove unnecessary test files

--- backend/test_api.py
+++
@@ -1,4 +0,0 @@
-import requests
-
-url = "http://localhost:8000/profile/analytics/report"
-# I need an auth token to call this API.
--- backend/test_env.py
+++
@@ -1,5 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-print("KEY:", os.getenv("GROQ_API_KEY"))
